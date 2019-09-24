ecba39df6616fa5635cd50c3262023728556ddc4
sh1ft <pmayzenberg@gmail.com>
2019-09-24 12:34:27 +0300

    Initial Commit

---
+++ app/src/test/java/com/pm/mysuperstoreapp/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+package com.pm.mysuperstoreapp;
+
+
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+class ExampleUnitTest {
+    @Test
+    void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
