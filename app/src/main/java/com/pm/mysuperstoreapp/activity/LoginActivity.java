011ded331557a905751fa0717cd0ff4eef7bda62
sh1ft <pmayzenberg@gmail.com>
2019-10-10 20:36:27 +0300

    * Register user broken

--- app/src/main/java/com/pm/mysuperstoreapp/activity/LoginActivity.java
+++ app/src/main/java/com/pm/mysuperstoreapp/activity/LoginActivity.java
@@ -3,11 +3,15 @@ package com.pm.mysuperstoreapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.pm.mysuperstoreapp.R;
 
 
@@ -20,12 +24,54 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
+    }
     public void login(View view) {
 
+        switch(view.getId()) {
+            case R.id.activity_login_btn_email_login:
+                break;
+            case R.id.activity_login_btn_google_login:
+                break;
+            case R.id.activity_login_btn_facebook_login:
+                break;
+            case R.id.activity_login_btn_forgot_password:
+                break;
+            case R.id.activity_login_btn_register:
+                Button b = (Button) view;
+                b.setTextColor(Color.parseColor("#C0C0C0"));
+                final Intent intent = new Intent(getApplicationContext(),
+                        RegisterActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_in_right);
+                finish();
+                break;
+            default:
+                break;
+        }
     }
+
+    private void emailLogin(View view) {
+
+    }
+
+    private void googleLogin(View view) {
+
+    }
+
+    private void facebookLogin(View view) {
+
+    }
+
+
 }
--- app/src/main/java/com/pm/mysuperstoreapp/activity/RegisterActivity.java
+++ app/src/main/java/com/pm/mysuperstoreapp/activity/RegisterActivity.java
@@ -1,24 +1,83 @@
 package com.pm.mysuperstoreapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Bundle;
-import android.view.View;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.pm.mysuperstoreapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText email;
+    private EditText password;
+    private String firstname;
+    private String lastname;
+    private TextView notification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.activity_register_et_email);
+        password = findViewById(R.id.activity_login_et_password);
+        notification = findViewById(R.id.activity_register_tv_notification);
     }
 
     public void registerNewUser(View view) {
+
+        mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "REG_OK", Toast.LENGTH_SHORT).show();
+                           /* final Intent intent = new Intent(getApplicationContext(),
+                                    MainActivity.class);
+
+                            startActivity(intent);
+                            finish();*/
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                           // Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    //Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            //notification.setText(task.getException().toString());
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     public void cancelRegistration(View view) {
+        final Intent intent = new Intent(getApplicationContext(),
+                LoginActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_in_left);
+        finish();
+
+    }
+
+    public void saveUserToDB(FirebaseUser user) {
+
     }
 }
